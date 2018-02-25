1af94d7c0cfdf6a0b765de9babec7ddaf00f10e5
Thomas Vincent <vrince@gmail.com>
2018-02-24 22:38:23 -0500

    using simpler webpack template and main vs receiver apps in the same build

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
+      persistent
+      :mini-variant="miniVariant"
       :clipped="clipped"
       v-model="drawer"
+      enable-resize-watcher
       fixed
       app
     >
       <v-list>
         <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
+          value="true"
           v-for="(item, i) in items"
-          exact
+          :key="i"
         >
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
@@ -24,50 +24,40 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>web</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>remove</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>menu</v-icon>
       </v-btn>
     </v-toolbar>
     <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <router-view/>
     </v-content>
     <v-navigation-drawer
       temporary
       :right="right"
       v-model="rightDrawer"
       fixed
+      app
     >
       <v-list>
-        <v-list-tile @click.native="right = !right">
+        <v-list-tile @click="right = !right">
           <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
+            <v-icon>compare_arrows</v-icon>
           </v-list-tile-action>
           <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
         </v-list-tile>
@@ -80,21 +70,22 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vue-Sender'
     }
-  }
+  },
+  name: 'App'
+}
 </script>
---
+++ src/Receiver.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-toolbar app>
+      <v-icon>query_builder</v-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color='error'>Button</v-btn>
+    </v-toolbar>
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Vue-Receiver'
+    }
+  },
+  name: 'App'
+}
+</script>
